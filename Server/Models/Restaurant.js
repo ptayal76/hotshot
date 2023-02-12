ec2b7818becbe9af496ce70cb629b4a9e1c9c38b
Aditya Agrawal <95185307+cheekuag@users.noreply.github.com>
2023-02-12 23:17:32 +0530

    Added all Restaurant Models

---
+++ Server/Models/Restaurant.js
@@ -0,0 +1,90 @@
+const Dish=require('./Dish')
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const restaurant = new schema({
+    ownerName: {
+        required: true,
+        type: String
+    },
+    restaurantName: {
+        required: true,
+        type: String
+    },
+    phoneNumber: {
+        required: true,
+        type: String
+    },
+    email: {
+        required: true,
+        type: String
+    },
+    menu: [
+        {
+            type: mongoose.Schema.Types.ObjectId, ref: `Dish`,
+        }
+    ],
+    pic: {
+        data: Buffer,
+        contentType: String
+    },
+    location: {
+        required: true,
+        type: String
+    },
+    usersRated:[
+        {
+            id:{type:mongoose.Schema.Types.ObjectId, ref: `User`},
+            rate:{type:Number},
+        }
+    ],
+    rating: {
+        type: Number
+    },
+    timing: [
+        {
+            start_time: Date
+        },
+        {
+            end_time: Date
+        }
+    ],
+    status: {
+        type: String,
+        enum: ['on', 'off'],
+        required:true 
+    },
+    order_types: {
+
+        notCollected: [
+            {
+                type: String
+            }
+        ]
+        ,
+
+        collected: [
+            {
+                type: String
+            }
+        ]
+
+        ,
+        rejected: [
+            {
+                type: String
+            }
+        ]
+
+        ,
+        pending: [
+            {
+                type: String
+            }
+        ]
+
+    }
+
+})
+
+module.exports = mongoose.model('Restaurant', restaurant);
