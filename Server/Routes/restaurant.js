ee6def0f6f51f9b37aef431856a61d016003e354
vd-coder <dawravaibhav@gmail.com>
2023-02-18 02:59:30 +0530

    minor changes

--- Server/Routes/restaurant.js
+++ Server/Routes/restaurant.js
@@ -21,15 +21,17 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 //GET ALL RESTAURANTS BY QUERY
+
 router.get('/food/rest', async (req, res) => {
   try {
     var obj = {};
     if (req.query.status) obj.status = req.query.status
     if (req.query.rating) obj.rating = { $gte: req.query.rating }
     if (req.query.locationCategory) obj.locationCategory = req.query.locationCategory;
-    if (req.query.name) {
-      obj.restaurantName = {
-        "$regex": `${req.query.name}`, "$options": 'i'
+    if(req.query.name)
+    {
+      obj.restaurantName={
+        "$regex":`${req.query.name}`,"$options":'i'
       }
     }
     const restaurants = await Restaurant.find(obj);
@@ -40,14 +42,13 @@ router.get('/food/rest', async (req, res) => {
 });
 
 
+
+
 //GET A RESTAURANT BY ID
 router.get('/food/rest/:restid', async (req, res) => {
   try {
     const id = req.params.restid;
     const restaurant = await Restaurant.findById(id);
-    if (!restaurant) {
-      return res.status(400).json('No restaurant by this id found!');
-    }
     return res.json(restaurant);
   } catch (err) {
     return res.status(400).send(err.message);
@@ -57,15 +58,16 @@ router.get('/food/rest/:restid', async (req, res) => {
 //CREATE A RESTAURANT
 router.post('/food/rest', upload.single('pic'), async (req, res) => {
   try {
-    console.log(req.body);
-    var existingRest = await Restaurant.findOne({ email: req.body.email });
-    if (!existingRest) {
+    console.log(req.body,"register");
+    var existingRest = await Restaurant.findOne({email:req.body.email});
+    if(!existingRest) {
+      JSON.parse(req.body.razorpayCred)
       const restaurant = new Restaurant(req.body);
       if (req.file) {
         restaurant.pic.data = req.file.buffer;
         restaurant.pic.contentType = req.file.mimetype;
       }
-      existingRest = restaurant;
+      existingRest= restaurant;
       restaurant.save();
     }
     jwt.sign(
@@ -80,73 +82,103 @@ router.post('/food/rest', upload.single('pic'), async (req, res) => {
     return res.status(400).send(err.message);
   }
 });
-
-//LOGIN FOR RESTAURANT
-router.post('/food/rest/login', async (req, res) => {
-  try {
-    const restaurant = await Restaurant.findOne({ email: req.body.email });
-    if (!restaurant) {
-      return res.status(403).json({ message: "You are not registered" });
-    }
-    else {
-      jwt.sign(
-        { isowner: true, id: restaurant._id },
-        process.env.JWT_SEC,
-        (err, token) => {
-          res.header('token', `${token}`);
-          return res.json(restaurant);
-        }
-      );
-    }
+//login for restaurant 
+router.post('/food/rest/login',async (req,res)=>{
+  try{
+    
+    console.log(req.body,'login');
+  const restaurant=await Restaurant.findOne({email:req.body.email});
+  if(!restaurant)
+  {
+    return res.status(403).json({message:"You are not registered"});
   }
-  catch (err) {
+  else
+  {
+    jwt.sign(
+      { isowner: true, id: restaurant._id },
+      process.env.JWT_SEC,
+      (err, token) => {
+        res.header('token', `${token}`);
+        return res.json(restaurant);
+      }
+    );
+  }
+  }
+  catch(err)
+  {
     res.status(403).send(err.message);
   }
 })
-
 //UPDATE A RESTAURANT BY ID
-router.put('/food/rest/:restid', verifyToken, authenticateOwner, authorizeOwner, async (req, res) => {
-  try {
-    const restaurant = await Restaurant.findByIdAndUpdate(
-      req.params.restid,
-      req.body,
-      { runValidators: true, new: true }
-    );
-    return res.json(restaurant);
-  } catch (err) {
-    return res.status(400).send(err.message);
+router.put(
+  '/food/rest/:restid',
+  verifyToken,
+  authenticateOwner,
+  authorizeOwner,
+  async (req, res) => {
+    try {
+      const restaurant = await Restaurant.findByIdAndUpdate(
+        req.params.restid,
+        req.body,
+        { runValidators: true, new: true }
+      );
+
+      return res.json(restaurant);
+    } catch (err) {
+      return res.status(400).send(err.message);
+    }
   }
-}
 );
 
-//RATE A RESTAURANT 
-router.put('/food/rest/rate/:restid', verifyToken, authenticateUser, async (req, res) => {
-  try {
-    const restaurant = await Restaurant.findById(req.params.restid);
-    if (!restaurant) {
-      return res.status(400).json('Wrong RestId');
-    }
-    const user = await User.findById(req.user);
-    var found = 0;
-    for (let i = 0; i < restaurant.usersRated.length; i++) {
-      if (restaurant.usersRated[i].id == user._id) {
-        found = 1;
-        restaurant.rating = (restaurant.rating * restaurant.usersRated.length - restaurant.usersRated[i].rate + req.body.rating) / restaurant.usersRated.length;
-        restaurant.usersRated[i].rate = req.body.rating;
+// rate a restaurant
+router.put(
+  '/food/rest/rate/:restid',
+  verifyToken,
+  authenticateUser,
+  authorizeUser,
+  async (req, res) => {
+    try {
+      const restaurant = await Restaurant.findById(req.params.restid);
+      const user = await User.findById(req.user);
+      var found = 0;
+      for (let i = 0; i < restaurant.usersRated.length; i++) {
+        if (restaurant.usersRated[i].id == user._id) {
+          found = 1;
+          restaurant.rating =
+            (restaurant.rating * restaurant.usersRated.length -
+              restaurant.usersRated[i].rate +
+              req.rating) /
+            restaurant.usersRated.length;
+          restaurant.usersRated[i].rate = req.rating;
+        }
       }
-    }
-    if (!found) {
-      restaurant.rating = (restaurant.rating * restaurant.usersRated.length + req.body.rating) / (restaurant.usersRated.length + 1);
-      restaurant.usersRated.push({ id: user._id, rate: req.body.rating });
+      if (!found) {
+        restaurant.rating =
+          (restaurant.rating * restaurant.usersRated.length + req.rating) /
+          (restaurant.usersRated.length + 1);
+        restaurant.usersRated.push({ id: user._id, rate: req.rating });
+      }
       restaurant.save();
-      return res.status(200).json(restaurant);
-    } else {
-      return res.status(200).json("You have already rated");
+    } catch (err) {
+      return res.status(400).send(err.message);
     }
-  } catch (err) {
-    return res.status(400).send(err.message);
   }
-}
+);
+
+//DELETE A RESTAURANT
+router.delete(
+  '/food/rest/:restid',
+  verifyToken,
+  authenticateOwner,
+  authorizeOwner,
+  async (req, res) => {
+    try {
+      await Restaurant.findByIdAndDelete(req.params.restid);
+      return res.status(200).json('Restaurant has been deleted!');
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  }
 );
 
 //BUSINESS INSIGHTS
@@ -167,13 +199,13 @@ router.get('/stats', verifyToken, authenticateOwner, async (req, res) => {
             month: { $month: "$timeOfOrder" },
             day: { $dayOfMonth: "$timeOfOrder" }
           },
-          total: { $sum: '$total' }
-        }
+          total : {$sum : '$total'}
+      }
       },
     ]);
-    return res.status(200).json(data);
+    res.status(200).json(data);
   } catch (err) {
-    return res.status(500).json(err);
+    res.status(500).json(err);
   }
 })
 
